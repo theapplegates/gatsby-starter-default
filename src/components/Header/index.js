2cd68de635498ded3a9f662b40032b375ba04127
Paul <paul@theapplegates.com>
2020-06-04 06:58:32 -0400

    Create index.js

---
+++ src/components/Header/index.js
@@ -0,0 +1,20 @@
+import React from "react"
+
+import { Container, Logo } from "./styles"
+import Nav from "../Nav"
+import Search from "../Search"
+
+const searchIndices = [
+  { name: `Pages`, title: `Pages`, hitComp: `PageHit` },
+  { name: `Posts`, title: `Blog Posts`, hitComp: `PostHit` },
+]
+
+const Header = ({ site, transparent }) => (
+  <Container transparent={transparent}>
+    <Logo to="/" title={site.title} rel="home" />
+    <Nav />
+    <Search collapse indices={searchIndices} />
+  </Container>
+)
+
+export default Header
